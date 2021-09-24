761128ea9a03ad0181f84d3ca481bff4065ecf72
alexanderdelfim <alexanderdelfimjr11@gmail.com>
2021-09-23 22:55:55 -0300

    Adicionando coneção com Mongoose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const dotenv = require('dotenv/config');
 const express =  require('express');
 const cors =  require('cors');
+const mongoose = require('mongoose');
 
 const routes =  require('./routes/index');
+
+mongoose.connect(' mongodb://127.0.0.1:27017/crud_api_node');
 
 const app = express();
 
